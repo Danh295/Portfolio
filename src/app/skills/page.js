8753a44237d4d48b37c8d9bc8bb21c4785497bed
Danny <79547297+Danh295@users.noreply.github.com>
2025-08-20 16:09:51 -0400

    Home Page + Temp Msgs Fixes #6

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function SkillsPage() {
   return (
-    <div>SkillsPage</div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <h1>Skills Page</h1>
+      <p>Sorry! coming soon...</p>
+    </div>
   )
 }
